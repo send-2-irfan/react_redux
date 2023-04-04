6985a83c55a3272b629b34904710e6b3410aaa4d
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-04 07:05:10 +0500

    header component renderd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './containers/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from Dalle</h1>
+      <Header />
     </div>
   );
 }
