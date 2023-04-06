c029d00711b08cb8ec55f95199913d031b310344
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-06 21:57:58 +0500

    done with productDetails

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -12,10 +12,11 @@ function ProductDetails() {
   const product = useSelector((state) => state.product);
   const { productId } = useParams();
   console.log(product);
+  const { image, title, price, category, description } = product;
 
   const fetchProductDetails = async () => {
     const response = await axios
-      .get("https://fakestoreapi.com/products/1")
+      .get(`https://fakestoreapi.com/products/${productId}`)
       .catch((err) => console.log(err));
 
     dispatch(selectedProduct(response.data));
@@ -28,7 +29,34 @@ function ProductDetails() {
 
   return (
     <div>
-      <h1>ProductDetails</h1>
+      {Object.keys(product).length == 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h4 className="ui brown block header">{category}</h4>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidder content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
